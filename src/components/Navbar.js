174021cc2830006cc1a00d1e6bebcfeaecb8151f
Clerk3 <Clerk3@local.minhaslawyers.ca>
2021-03-23 17:55:35 -0400

    creating navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React,{useState} from 'react'
+import {Button} from './Button';
+import './Navbar.css';
+import Dropdown from './Dropdown';
+import {Link} from 'react-router-dom';
+function Navbar() {
+    const [click, setClick]= useState(false);
+
+    const handleClick = () => setClick(!click);
+
+    const closeMobileMenu = () =>setClick(false);
+    return (
+        <nav className="navbar">
+            <Link to="/"className="navbar-logo">
+                LOGO
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+            </div>
+            <ul className={click ? 'nav-menu active' :'nav-menu'}>
+            <li className='nav-item'>
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+                Services<i className='fas fa-caret-down'/>
+                </Link>
+                {dropdown && <Dropdown/>}
+            </li>
+            <li className='nav-item'>
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+                </Link>
+            </li>
+            <li className='nav-item'>
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+                </Link>
+            </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
